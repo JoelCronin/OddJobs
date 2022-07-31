ee17327f35bdaef3edb18e415cb358c369ead9fc
chirstiankaikai <ckaikai19@gmail.com>
2022-07-30 23:57:25 -0400

    fixed bug

--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from "react";
-import "../styles/AccountStyles/Account-Header.css"
+import "../styles/AccountStyles/Account-Header.css";
+import { Link } from 'react-router-dom';
 
 
 import Auth from '../utils/auth';
